7ae89cc854cb7ea53f7a47a70a3e82ca2c965377
Fábio Vito <fabiovito@hotmail.com>
2019-11-15 13:46:18 -0300

    Add funcoes retira() e print_stack()

--- trabalho_aula/minha_pilha_estatica.c
+++ trabalho_aula/minha_pilha_estatica.c
@@ -4,8 +4,12 @@
 
 typedef struct STACK{
     int n_elementos;
-    int elementos[10];
+    int elementos[5];
 }Stack;
+
+void inicializa(Stack *ptr_stack){
+    ptr_stack->n_elementos = 0;
+}
 
 void insere(Stack *ptr_stack, int elemento){
     int maximo = (sizeof(ptr_stack->elementos) / sizeof(int));
@@ -14,29 +18,52 @@ void insere(Stack *ptr_stack, int elemento){
         ptr_stack->elementos[ptr_stack->n_elementos] = elemento;
         ptr_stack->n_elementos++;
     }
-    
+    else{
+        printf("Numero maximo de elementos atingido.\n");
+    }    
 }
 
-void inicializa(Stack *ptr_stack){
-    ptr_stack->n_elementos = 0;
+void retira(Stack *ptr_stack){
+    if(ptr_stack->n_elementos > 0){
+        ptr_stack->n_elementos --;
+    }
+    else{
+        printf("Impossivel retirar elemento \nPilha vazia.\n");
+    }
 }
 
-int main(int argc, char const *argv[])
-{
+void print_stack(Stack *ptr_stack){
+    for (int i = 0; i < ptr_stack->n_elementos; i ++){
+        printf("Elemento %d: %d\n", i, ptr_stack->elementos[i]);
+    }
+    if(ptr_stack->n_elementos == 0){
+        printf("Pilha vazia.\n");
+    }
+}
+
+int main(int argc, char const *argv[]){
     Stack stack;
-    Stack *pilha = &stack;
 
     inicializa(&stack);
+    insere(&stack, 2);
+    retira(&stack);
+    insere(&stack, 15);
+    insere(&stack, 15);
+    retira(&stack);
+    retira(&stack);
+    retira(&stack);
+    insere(&stack, 15);
+    insere(&stack, 5);
+    insere(&stack, 51);
+    insere(&stack, 89);
+    retira(&stack);
+    insere(&stack, 40);
+    insere(&stack, 45);
+    insere(&stack, 72);
+    
+    print_stack(&stack);
 
-    insere(&stack, 3);
-    insere(pilha, 75);
-    insere(pilha, 5);
 
-    printf("\n\nElemento: %d\n", stack.elementos[1]);
-    printf("elementos %i\n",stack.n_elementos);
-    printf("stack %i\n",stack);
-    printf("pilha %i\n",*pilha);
-    printf("&stack %i\n",&stack);
 
     return 0;
 }
