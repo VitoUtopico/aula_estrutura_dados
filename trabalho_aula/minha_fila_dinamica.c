c38f714d03f2f9c3e8b6fb3faf2408979d218238
Fábio Vito <fabiovito@hotmail.com>
2019-11-16 17:29:36 -0300

    Add inicializa()

--- trabalho_aula/minha_fila_dinamica.c
+++ trabalho_aula/minha_fila_dinamica.c
@@ -1,21 +1,21 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-struct Fila{
-    int primeiro;
-    int ultimo;
-    int n_itens;
-    int *dados;
-};
+typedef struct ELEMENTO{
+    int dado;
+    struct ELEMENTO *proximo;
+}Elemento;
 
-void inicializa_fila(struct Fila *fila, int espacos){
-    fila->dados =  (int*) malloc (espacos * sizeof(int));
+typedef struct FILA{
+    Elemento *inicio;
+    Elemento *fim;
+}Fila;
 
+void inicializa(Fila *fila){
+    fila->inicio = NULL;
+    fila->fim = NULL;
 }
 
-void inserir(struct Fila *fila, int valor){
-    fila->dados[fila->primeiro + fila->n_itens] = valor;
-}
 
 int main(int argc, char *argv[]){
 
